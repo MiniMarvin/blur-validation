8ec007e0d2f8bf8799f91e93f04810c670886330
Caio Gomes <caiogomes34@gmail.com>
2021-08-21 16:10:41 +0000

    initial implementation for detect blur

---
+++ main.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import imutils
+
+def show_comparative_images(image, magnitude):
+  (fig, ax) = plt.subplots(1, 2, )
+  ax[0].imshow(image, cmap="gray")
+  ax[0].set_title("Input")
+  ax[0].set_xticks([])
+  ax[0].set_yticks([])
+
+  ax[1].imshow(magnitude, cmap="gray")
+  ax[1].set_title("Magnitude Spectrum")
+  ax[1].set_xticks([])
+  ax[1].set_yticks([])
+  plt.show()
+	
+def detect_blur_fft(image, radius_from_center=60, thresh=10):
+  (h, w) = image.shape
+  (cX, cY) = (int(w / 2.0), int(h / 2.0))
+
+  # compute the FFT to find the frequency transform, then shift
+  # the zero frequency component (i.e., DC component located at
+  # the top-left corner) to the center where it will be more
+  # easy to analyze
+  fft = np.fft.fft2(image)
+  fftShift = np.fft.fftshift(fft)
+
+  # compute the magnitude spectrum of the transform
+  magnitude = 20*np.log(np.abs(fftShift))
+  # display the original input image
+  show_comparative_images(image, magnitude)
+
+  # zero-out the center of the FFT shift (i.e., remove low
+  # frequencies), apply the inverse shift such that the DC
+  # component once again becomes the top-left, and then apply
+  # the inverse FFT
+  fftShift[cY - radius_from_center:cY + radius_from_center, cX - radius_from_center:cX + radius_from_center] = 0
+  fftShift = np.fft.ifftshift(fftShift)
+  recon = np.fft.ifft2(fftShift)
+
+  # compute the magnitude spectrum of the reconstructed image,
+  # then compute the mean of the magnitude values
+  magnitude = 20 * np.log(np.abs(recon))
+
+  show_comparative_images(image, magnitude)
+
+  mean = np.mean(magnitude)
+  # the image will be considered "blurry" if the mean value of the
+  # magnitudes is less than the threshold value
+  return (mean, mean <= thresh)
