6cbad6e447df362bb5e297332f008331798ba5c0
Caio Gomes <caiogomes34@gmail.com>
2021-08-22 11:44:12 +0000

    setup blur circle filter

--- src/frequency.py
+++ src/frequency.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def return_to_spatial_domain(spectrum):
 	"""
@@ -21,10 +22,39 @@ def compute_magnitude(frequency_image):
 	magnitude = 20*np.log(np.abs(frequency_image))
 	return magnitude
 
-def high_pass_filter(frequency_image, frequency_radius):
+def square_high_pass_filter(frequency_image, frequency_radius):
   (height, width) = frequency_image.shape
   (center_x, center_y) = width // 2, height // 2
 
   frequency_image[center_y - frequency_radius:center_y + frequency_radius, center_x - frequency_radius:center_x + frequency_radius] = 0
 
   return frequency_image
+
+def circle_high_pass_filter(frequency_image, frequency_radius):
+  (height, width) = frequency_image.shape[:2]
+  (center_x, center_y) = width // 2, height // 2
+  
+  Y, X = np.ogrid[:height, :width]
+
+  dist_from_center = np.sqrt((X - center_x)**2 + (Y - center_y)**2)
+
+  mask = dist_from_center <= frequency_radius
+  frequency_image[mask] = 0
+
+  return frequency_image
+
+def circle_smooth_high_pass_filter(frequency_image, frequency_radius):
+  (height, width) = frequency_image.shape[:2]
+  (center_x, center_y) = width // 2, height // 2
+
+  filter_mask = np.full((height, width), 255, dtype = np.uint8)
+  filter_mask = cv2.circle(filter_mask, 
+    (center_x, center_y), 
+    frequency_radius, 0, -1)
+    
+  kernel_size = frequency_radius//10
+  filter_mask = cv2.GaussianBlur(filter_mask,
+  (kernel_size,kernel_size),0)/255.0
+  masked_image = frequency_image * filter_mask
+
+  return masked_image
--- src/image_processing.py
+++ src/image_processing.py
@@ -11,36 +11,39 @@ def gray_image(img_path):
 	return gray
 
 def detect_blur_fft(image, frequency_threshold=60, file_name="test"):
-	"""
-	computes if the image is blurry (i.e. a low definition image), by processing the image with a high pass filter on frequency spectrum
-	"""
-	base_path = "output_images/"
-	path = base_path + file_name + "/" + "spatial_domain_1.png"
-	mf.ensure_dir(path)
-	cv2.imwrite(path, image)
+  """
+  computes if the image is blurry (i.e. a low definition image), by processing the image with a high pass filter on frequency spectrum
+  """
+  base_path = "output_images/"
+  path = base_path + file_name + "/" + "spatial_domain_1.png"
+  mf.ensure_dir(path)
+  cv2.imwrite(path, image)
 
-	fft_image = frequency.go_to_frequency_domain(image)
-	magnitude = frequency.compute_magnitude(fft_image)
+  fft_image = frequency.go_to_frequency_domain(image)
+  magnitude = frequency.compute_magnitude(fft_image)
 
-	path = base_path + file_name + "/" + "frequency_domain_1.png"
-	mf.ensure_dir(path)
-	cv2.imwrite(path, magnitude)
+  path = base_path + file_name + "/" + "frequency_domain_1.png"
+  mf.ensure_dir(path)
+  cv2.imwrite(path, magnitude)
 
-	filtered_spectrum = frequency.high_pass_filter(
-		fft_image, frequency_threshold)
-	
-	filtered_magnitude = frequency.compute_magnitude(filtered_spectrum)
+  # filtered_spectrum = frequency.square_high_pass_filter(fft_image, frequency_threshold)
 
-	path = base_path + file_name + "/" + "frequency_domain_2.png"
-	mf.ensure_dir(path)
-	cv2.imwrite(path, filtered_magnitude)
+  # filtered_spectrum = frequency.circle_high_pass_filter(fft_image, frequency_threshold)
+
+  filtered_spectrum = frequency.circle_smooth_high_pass_filter(fft_image, frequency_threshold)
+
+  filtered_magnitude = frequency.compute_magnitude(filtered_spectrum)
+
+  path = base_path + file_name + "/" + "frequency_domain_2.png"
+  mf.ensure_dir(path)
+  cv2.imwrite(path, filtered_magnitude)
     
-	recon_image = frequency.return_to_spatial_domain(filtered_spectrum)
-	recon_magnitude = frequency.compute_magnitude(recon_image)
+  recon_image = frequency.return_to_spatial_domain(filtered_spectrum)
+  recon_magnitude = frequency.compute_magnitude(recon_image)
 
-	path = base_path + file_name + "/" + "spatial_domain_2.png"
-	mf.ensure_dir(path)
-	cv2.imwrite(path, recon_magnitude)
+  path = base_path + file_name + "/" + "spatial_domain_2.png"
+  mf.ensure_dir(path)
+  cv2.imwrite(path, recon_magnitude)
 
-	recon_mean = np.mean(recon_magnitude)
-	return recon_mean
+  recon_mean = np.mean(recon_magnitude)
+  return recon_mean
